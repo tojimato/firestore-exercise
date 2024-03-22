aa993ee6325dea201855a31306785bbc2d6688b2
tojimato <tojimato@gmail.com>
2024-03-22 14:08:00 +0300

    initial project cleaned

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders project title on main page', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Firestore query examples/i);
   expect(linkElement).toBeInTheDocument();
 });
